d3d3c2264c32f214d8258b09526ac14848f23fa8
luizfelipew <luiz.f.campos@partner.bmw.com.br>
2023-11-18 12:22:31 -0300

    :sparkles: adding new tests using Karate and created docker compose.

--- kanbanboard/src/main/java/org/kanbanboard/kanbanboard/write/adapter/in/BucketController.java
+++ kanbanboard/src/main/java/org/kanbanboard/kanbanboard/write/adapter/in/BucketController.java
@@ -3,24 +3,28 @@ package org.kanbanboard.kanbanboard.write.adapter.in;
 import lombok.RequiredArgsConstructor;
 import org.kanbanboard.kanbanboard.write.domain.application.CreateBucketCommand;
 import org.kanbanboard.kanbanboard.write.domain.application.CreateBucketCommandHandler;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+
 @RestController
-@RequestMapping("v1/buckets")
+@RequestMapping(value = "v1/buckets")
 @RequiredArgsConstructor
 public class BucketController {
 
     private final CreateBucketCommandHandler handler;
 
-    @PostMapping
-    public ResponseEntity create(final CreateBucketCommand command) {
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity create(@RequestBody final CreateBucketCommand command) throws Exception {
 
         handler.handle(command);
 
-        return ResponseEntity.noContent()
+        return ResponseEntity.created(new URI(""))
                 .build();
     }
 
--- kanbanboard/src/main/java/org/kanbanboard/kanbanboard/write/domain/application/CreateBucketCommand.java
+++ kanbanboard/src/main/java/org/kanbanboard/kanbanboard/write/domain/application/CreateBucketCommand.java
@@ -1,7 +1,13 @@
 package org.kanbanboard.kanbanboard.write.domain.application;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.UUID;
 
-public record CreateBucketCommand(UUID id, int position, String name) {
+public record CreateBucketCommand(
+        @JsonProperty("id") UUID id,
+        @JsonProperty("position") Integer position,
+        @JsonProperty("name") String name
+) {
 
 }
